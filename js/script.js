bd8a29d0eaee233ae59f37e2193202b8b7d62207
Leonardo <lvneves2010@gmail.com>
2020-07-15 15:49:19 -0300

    filter search

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $ = jQuery;
 var json = $.getJSON("./js/eventos.json", function (results) {
   $.each(results, function () {
-    var div = `<div class="ibm-grid-col-xlg-16-4 ibm-grid-col-lg-16-8 ibm-grid-col-md-8-4 ibm-grid-col-sm-4-4  ibm-padding-top-1 ibm-padding-bottom-1">
+    var div = `<div class="ibm-grid-col-xlg-16-4 ibm-grid-col-lg-16-8 ibm-grid-col-md-8-4 ibm-grid-col-sm-4-4  ibm-padding-top-1 ibm-padding-bottom-1" keyword="${this.keyword}">
                       <div class="ibm-background-neutral-white-35"><img src="${this.image}" class="ibm-resize" alt="${this.alt}">
                        <div class="ibm-padding-content">
                        <p class="ibm-type-d">${this.data}</p>
@@ -17,7 +17,7 @@ var json = $.getJSON("./js/eventos.json", function (results) {
 });
 var json = $.getJSON("./js/ondemand.json", function (results) {
   $.each(results, function () {
-    var div = `<div class="ibm-grid-col-xlg-16-4 ibm-grid-col-lg-16-8 ibm-grid-col-md-8-4 ibm-grid-col-sm-4-4  ibm-padding-top-1 ibm-padding-bottom-1">
+    var div = `<div class="ibm-grid-col-xlg-16-4 ibm-grid-col-lg-16-8 ibm-grid-col-md-8-4 ibm-grid-col-sm-4-4  ibm-padding-top-1 ibm-padding-bottom-1" keyword="${this.keyword}">
                       <div class="ibm-background-gray-35"><img src="${
       this.image
       }" class="ibm-resize" alt="${this.alt}">
@@ -58,6 +58,81 @@ for (i = 0; i < card.length; i++) {
     } else {
       card[i].style.display = "none";
     }
+  }
+}
+
+function resetFilter() {
+  document.getElementById('checkbox-cloud').checked = false;
+  document.getElementById('checkbox-ia').checked = false;
+  document.getElementById('checkbox-seguridad').checked = false;
+  document.getElementById('checkbox-infraestructura').checked = false;
+  document.getElementById('checkbox-data').checked = false;
+  document.getElementById('checkbox-consultoria').checked = false;
+  document.getElementById('checkbox-servicios').checked = false;
+  check()
+}
+
+function check()
+{
+
+  var keywords = [];
+  if (document.getElementById('checkbox-cloud').checked) 
+  {
+    keywords.push("cloud")
+  }
+  if (document.getElementById('checkbox-ia').checked) 
+  {
+    keywords.push("ia")
+  }
+  if (document.getElementById('checkbox-seguridad').checked) 
+  {
+    keywords.push("seguridad")
+  }
+  if (document.getElementById('checkbox-infraestructura').checked) 
+  {
+    keywords.push("infraestructura")
+  }
+  if (document.getElementById('checkbox-data').checked) 
+  {
+    keywords.push("data")
+  }
+  if (document.getElementById('checkbox-consultoria').checked) 
+  {
+    keywords.push("consultoria")
+  }
+  if (document.getElementById('checkbox-servicios').checked) 
+  {
+    keywords.push("servicios")
+  }
+
+  filterSearch(keywords)
+  console.log("keywords", keywords)
+}
+
+function filterSearch(keys) {
+  var i, card, container;
+  container = document.getElementById("cards");
+  console.log("keys", keys)
+  card = container.getElementsByClassName("ibm-padding-top-1");
+
+
+  if(keys.length == 0) {
+  for (i = 0; i < card.length; i++) {
+        card[i].style.display = "";
+    }
+  } else {
+    for (i = 0; i < card.length; i++) {
+          card[i].style.display = "none";
+      }
+    keys.map((key) => {
+      for (i = 0; i < card.length; i++) {
+        k = card[i].getAttribute("keyword");
+        console.log("k>>", k)
+        if( k == key) {
+          card[i].style.display = "";
+        }
+      }
+    })
   }
 }
 
